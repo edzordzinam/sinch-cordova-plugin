16432ad73bb1b262f421c2ad8155d617519f4dc5
Edzordzinam E Dagadu <developer@edzordzinam.me>
2018-04-18 10:22:14 +0000

    ios update

--- src/ios/HWPCall.h
+++ src/ios/HWPCall.h
@@ -1,8 +1,13 @@
 #import <Cordova/CDV.h>
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMessageComposeViewController.h>
 
-@interface HWPCall : CDVPlugin
+@interface HWPCall : CDVPlugin <MFMessageComposeViewControllerDelegate>
+
+@property(strong) NSString* callbackID;
 
 - (void) makeVOIPCall:(CDVInvokedUrlCommand*)command;
+- (void) sendSMS:(CDVInvokedUrlCommand*)command;
 
 @property (nonatomic, readwrite, strong) NSTimer *durationTimer;
 
--- src/ios/HWPCall.m
+++ src/ios/HWPCall.m
@@ -36,6 +36,55 @@ NSString* customerAddress;
     callUIViewController.customerAddress = customerAddress;
     
     [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:callUIViewController animated:YES completion:nil];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:@"success"];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+
+
+}
+
+
+- (void)sendSMS:(CDVInvokedUrlCommand *)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* phoneNumber = [command.arguments objectAtIndex:0];
+    NSString* textMessage = [command.arguments objectAtIndex:1];
+
+        self.callbackID = command.callbackId;
+        
+        if(![MFMessageComposeViewController canSendText]) {
+        NSMutableDictionary* returnInfo = [NSMutableDictionary dictionaryWithCapacity:2];
+            
+        [returnInfo setObject:@"SMS_FEATURE_NOT_SUPPORTED" forKey:@"code"];
+        [returnInfo setObject:@"SMS feature is not supported on this device" forKey:@"message"];
+                
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:returnInfo];
+
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+            return;
+        }
+        
+        MFMessageComposeViewController *composeViewController = [[MFMessageComposeViewController alloc] init];
+        composeViewController.messageComposeDelegate = self;
+
+        NSMutableArray *recipients = [[NSMutableArray alloc] init];
+        
+    [recipients addObject:phoneNumber];
+            
+    [composeViewController setBody:textMessage];
+            [composeViewController setRecipients:recipients];
+
+            [self.viewController presentViewController:composeViewController animated:YES completion:nil];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:@"success"];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 
 }
 
@@ -44,5 +93,32 @@ NSString* customerAddress;
 }
 
 
+// Handle the different situations of MFMessageComposeViewControllerDelegate
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result {
+    BOOL succeeded = NO;
+    NSString* errorCode = @"";
+    NSString* message = @"";
+    
+    switch(result) {
+        case MessageComposeResultSent:
+            succeeded = YES;
+            message = @"Message sent";
+            break;
+        case MessageComposeResultCancelled:
+            message = @"Message cancelled";
+            errorCode = @"SMS_MESSAGE_CANCELLED";
+            break;
+        case MessageComposeResultFailed:
+            message = @"Message Compose Result failed";
+            errorCode = @"SMS_MESSAGE_COMPOSE_FAILED";
+            break;
+        default:
+            message = @"Sms General error";
+            errorCode = @"SMS_GENERAL_ERROR";
+            break;
+    }
+    
+    [self.viewController dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
